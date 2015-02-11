ce52ee9a49ec405ca66e487357fff65a2c8204e3
ARIS <ARIS@ARIS-PC>
2015-02-11 12:00:44 +0900

    Test Commit

--- src/oftein/visibility/tapping/package-info.java
+++ src/oftein/visibility/tapping/package-info.java
@@ -2,7 +2,7 @@
  * 
  */
 /**
- * @author ARIS
- *
+ * @author ARIS CAHYADI RISDIANTO
+ * This Package used for Tapping Application as one of Flow Visibility Tools. 
  */
 package oftein.visibility.tapping;
